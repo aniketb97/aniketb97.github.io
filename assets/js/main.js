1b4fdb31e51e2aa5b39753ee1693f491d79116bf
aniketb97 <aniketbanginwar@yahoo.co.in>
2020-12-11 23:27:07 +0530

    solving some bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,10 +110,10 @@
     var typed_strings = $('.text-slider-items').text();
     var typed = new Typed('.text-slider', {
       strings: typed_strings.split(','),
-      typeSpeed: 80,
+      typeSpeed: 40,
       loop: true,
-      backDelay: 1100,
-      backSpeed: 30
+      backDelay: 850,
+      backSpeed: 20
     });
   }
 
